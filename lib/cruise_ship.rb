d5a92325df8270cad5787ccfe57e8dcc08412078
Sean Padden <sean.padden7@gmail.com>
2019-05-14 14:45:43 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,11 @@
 # }
 
 def select_winner(passengers)
-  winnier = ""
+  winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && 
       name.start_with?("A")
-      winnier = name
+      winner = name
     end
 end
 
